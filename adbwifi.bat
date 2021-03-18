baa2a9ee2c7a64fdbbac411219e3e6887fe54907
风筝 <jianglijs@163.com>
2021-03-18 10:16:37 +0800

    通过cmd命令连接Android wifi adb

---
+++ adbwifi.bat
@@ -0,0 +1,32 @@
+@echo off &setlocal enabledelayedexpansion
+::set your port
+set PORT_BASE=5555
+::list the device
+adb devices |findstr /i "\<device\>" >nul
+if "%errorlevel%" neq "0" (
+echo "device not found."
+goto :eof
+)
+::set devices serial
+for /f "tokens=1" %%i in ('adb devices^|findstr "\<device\>"') do (
+set device_serial=%%i
+echo
+)
+echo found devices %device_serial%
+
+::find IP for the phone
+::for /f "tokens=3 delims= " %%i in ('adb shell ifconfig wlan0') do (
+::set phone_ip=%%i
+::)
+FOR /F "tokens=9 delims= " %%a in ('adb shell ip route') do (
+	set phone_ip=%%a
+)
+echo device ip is %phone_ip%
+
+echo "Connecting......"
+adb -s %device_serial% tcpip %PORT_BASE%
+adb -s %device_serial% connect %phone_ip%:%PORT_BASE%
+
+echo Done
+adb devices -l
+ping -n 4 127.0.0.1 >&2 >nul
